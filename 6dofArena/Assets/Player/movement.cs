b713cbc9dd706edc410974b60160a9a066ece57f
waskerdu <rileywestonmiller@outlook.com>
2017-06-16 15:38:12 -0700

    Added Project
    I could use more comments but the code should be pretty self
    explanatory.

---
+++ 6dofArena/Assets/Player/movement.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+    public float torque = 0.1f;
+    public float force = 1.0f;
+    public float xForce = 0.0f;
+    public float yForce = 0.0f;
+    public float zForce = 0.0f;
+    public float yaw = 0.0f;
+    public float pitch = 0.0f;
+    public float roll = 0.0f;
+    public float breakForce = 100.0f;
+    public float rotBreak = 0.0f;
+    public float linBreak = 0.0f;
+    public bool isBreaking = false;
+    public float forwardThrustMultiplier = 2;
+    public float boostPower = 2;
+    public bool isBoosting = true;
+    public float lateralDampening = 0.1f;
+    Rigidbody myRb;
+
+    //states: braking, free
+    void startBreaking()
+    {
+        //myRb.drag = 100;
+        myRb.angularDrag = 100;
+        isBreaking = true;
+    }
+
+    void endBreaking()
+    {
+        //myRb.drag = 0;
+        myRb.angularDrag = 0;
+        isBreaking = false;
+    }
+
+
+    // Use this for initialization
+    void Start () {
+        myRb = gameObject.GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        xForce = Input.GetAxis("xThrust") * force;
+        yForce = Input.GetAxis("yThrust") * force;
+        zForce = Input.GetAxis("zThrust") * force * forwardThrustMultiplier;
+
+        yaw = Input.GetAxis("yaw") * torque;
+        pitch = Input.GetAxis("pitch") * torque;
+        roll = Input.GetAxis("roll") * torque;
+
+        linBreak = Input.GetAxis("break") * breakForce;
+        rotBreak = Input.GetAxis("break") * breakForce;
+        if (Input.GetButton("angBreak")) { linBreak = 0; rotBreak = breakForce; }
+        isBoosting = Input.GetButton("boost");
+
+        //if (Input.GetButtonDown("break")) { startBreaking(); }
+        //if (Input.GetButtonUp("break")) { endBreaking(); }
+    }
+
+    void FixedUpdate()
+    {
+        myRb.AddRelativeForce(Vector3.right * xForce);
+        myRb.AddRelativeForce(Vector3.down * yForce);
+        myRb.AddRelativeForce(Vector3.back * zForce);
+
+        myRb.AddRelativeTorque(Vector3.up * yaw);
+        myRb.AddRelativeTorque(Vector3.right * pitch);
+        myRb.AddRelativeTorque(Vector3.forward * roll);
+
+        myRb.drag = linBreak;
+        myRb.angularDrag = rotBreak;
+        if (isBoosting)
+        {
+            //kill lateral velocity over time while maintaining and increasing forward velocity
+            //desired velocity
+            Vector3 vDesired = transform.InverseTransformVector(myRb.velocity);//converts velocity to local space
+            vDesired.x = 0;
+            vDesired.y = 0;
+            if (vDesired.z < 0) { vDesired.z = 0; }
+            vDesired = transform.TransformVector(vDesired);//converts desired vec back into world space
+            myRb.velocity = Vector3.Slerp(myRb.velocity, vDesired, lateralDampening);
+            //if (vDesired.x < force) { myRb.AddRelativeForce()}
+            myRb.AddRelativeForce(Vector3.forward * boostPower);
+            myRb.angularDrag = breakForce;
+        }
+    }
+}
---
+++ 6dofArena/Assets/Turret/turretAi.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turretAi : MonoBehaviour {
+    public int state = 0;//state 0: passive search, 1: tracking, 2: firing, 3: active search
+    public Vector3 targetVec;
+    public Vector3 mountVec = Vector3.down;
+    public GameObject mount;
+    public GameObject player;
+    public GameObject gun;
+
+	void Start ()
+    {
+        //mount.transform.LookAt(mountVec+mount.transform.position);//anchors mount
+	}
+	
+	void Update ()
+    {
+        //.transform.LookAt(player.transform.position);
+        //mount.transform.LookAt(player.transform);
+        mount.transform.LookAt(2 * transform.position - player.transform.position);
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+}
